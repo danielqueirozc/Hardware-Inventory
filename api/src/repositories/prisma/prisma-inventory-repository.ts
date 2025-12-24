673b7a18ec40ec78839187108031f8630430a3b0
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-24 13:41:27 -0300

    feat: create getItemsQuantity function

--- api/src/repositories/prisma/prisma-inventory-repository.ts
+++ api/src/repositories/prisma/prisma-inventory-repository.ts
@@ -1,6 +1,6 @@
 import type { Item, ItemType, Prisma } from "../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
-import type { InventoryRepository } from "../inventory-repository";
+import type { GetItemsQuantityResponse, InventoryRepository } from "../inventory-repository";
 
 export class PrismaInventoryRepository implements InventoryRepository {
   async create(data: Prisma.ItemCreateInput): Promise<Item> {
@@ -17,5 +17,26 @@ export class PrismaInventoryRepository implements InventoryRepository {
     })
 
     return items
-   }
   }
+
+  async getItemsQuantity(): Promise<GetItemsQuantityResponse> {
+    try {
+      const itemsByType = await prisma.item.groupBy({
+        by: ['type'],
+        _count: {
+          id: true
+        }
+      })
+
+     const overview = itemsByType.reduce<Record<ItemType, number>>((acc, item) => {
+        acc[item.type] = item._count.id // acc[] = atribuindo a prop type no objeto que esta vazio de inicio, ex: component / = item._count.id esta atribuindo o valor da contagem
+        return acc
+      }, {} as Record<ItemType, number>) // Tipagem no valor inicial
+
+      return overview
+
+    } catch (error) {
+        throw new Error('Erro ao buscacar quantidade de itens')
+      }
+    }
+}
