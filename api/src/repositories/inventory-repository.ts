1ab87a43ab13fdd08ccb9eab1134ef0c048f1286
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-24 13:40:40 -0300

    feat : create getItemsQuantity type

--- api/src/repositories/inventory-repository.ts
+++ api/src/repositories/inventory-repository.ts
@@ -1,6 +1,16 @@
 import type { Item, ItemType, Prisma } from "../generated/prisma/client";
 
+export interface GetItemsQuantityResponse {
+  Component: number
+  Computer: number
+  Notebook: number
+  Materials: number
+  Cables: number
+}
+
 export interface InventoryRepository {
   create(data: Prisma.ItemCreateInput): Promise<Item>
   getByType(type: ItemType): Promise<Item[]>
+  getItemsQuantity(): Promise<GetItemsQuantityResponse>
+
 }
