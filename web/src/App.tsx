a00dec9548993c4e81cad48410a99aee003d7ea8
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-26 17:30:53 -0300

    feat: add routers

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
-import { Introduction } from "./ccomponents/introduction";
+import { Introduction } from "./components/introduction";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from "./components/login";
+import { Register } from "./components/register";
+import { Dashboard } from "./components/dashboard";
+import { Computer } from "./components/computer";
 
 export function App() {
   return (
-    <>
-      <Introduction />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Introduction />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/computer" element={<Computer />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
