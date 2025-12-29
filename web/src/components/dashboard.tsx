ae48a65ffb6830f3e656b78f522a5a3c5f47149d
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-29 14:58:12 -0300

    feat: add menu component

--- web/src/components/dashboard.tsx
+++ web/src/components/dashboard.tsx
@@ -1,10 +1,7 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+
 import { DashboardItem } from "./ui/dashboard-item"
 import { Cable, Component, Laptop, LaptopMinimal, Wrench } from "lucide-react"
+import { Menu } from "./menu"
 
 export function Dashboard() {
   return (
@@ -15,19 +12,7 @@ export function Dashboard() {
           <span className="text-white text-xs font-bold">Marina Bertioga</span>
         </div>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <button className="flex flex-col justify-around items-center `w-7.5 h-6.25 bg-transparent border-0 cursor-pointer focus:outline-none focus:shadow-none max-[481px]:w-6.25 max-[481px]:h-5">
-              <span className="h-0.75 w-full bg-gray-100 rounded-3xl transition-all duration-400 ease-in-out" />
-              <span className="h-0.75 w-full bg-gray-100 rounded-3xl transition-all duration-400 ease-in-out" />
-              <span className="h-0.75 w-full bg-gray-100 rounded-3xl transition-all duration-400 ease-in-out" />
-            </button>
-          </SheetTrigger>
-          <SheetContent>
-             
-          </SheetContent>
-        </Sheet>
-
+      <Menu />
       </header>
 
       <div className="flex flex-col py-10 gap-10 px-8 bg-gray-200 h-screen">
