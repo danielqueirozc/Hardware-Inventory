8cfd5854b746545a91e12a1dc3aa3fdd888b006d
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-29 14:54:10 -0300

    feat: create services and interceptors

---
+++ web/src/lib/axios.ts
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import type { LoginType, RegisterType } from '../@types'
+import { useAuthStore } from '@/context/auth-store'
+
+export const api = axios.create({
+    baseURL: 'http://localhost:3333',
+})
+
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().token
+
+   if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+api.interceptors.response.use(
+ (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Usa a action logout da própria store
+      useAuthStore.getState().logout()
+      window.location.href = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export const authServie = {
+  login: async ({ email, password}: LoginType) => {
+    const response = await api.post('/sessions', {email, password})
+
+   return response.data
+  },
+
+  register: async ({ name ,email, password}: RegisterType) => {
+    const response = await api.post('/users', {name, email, password})
+
+   return response.data
+  }
+}
