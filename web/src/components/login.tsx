42e826b8d0dfb59ebd4a68ab05cee017b0b26c0f
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-29 14:56:42 -0300

    feat: create functionalities

--- web/src/components/login.tsx
+++ web/src/components/login.tsx
@@ -1,7 +1,51 @@
+import { useAuthStore } from "@/context/auth-store";
 import { ArrowLeft } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
+  const [ error, setError ] = useState('')
+
+  const { login } = useAuthStore()
+
+  const navigate = useNavigate()
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  event.preventDefault()
+
+  const formData = new FormData(event.currentTarget)
+  const email = formData.get('email')
+  const password = formData.get('password')
+
+  if (!email) {
+    setError('Email é obrigatório')
+    return
+  }
+
+  if (!password) {
+    setError('Email é obrigatório')
+    return
+  }
+
+  try {
+    await login({
+      email: email as string,
+      password: password as string,
+    })
+
+    navigate('/dashboard')
+    
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      setError(error.response?.data?.error || 'Erro ao fazer login')
+    } else if (error instanceof Error) {
+      setError(error.message)
+    }
+  }
+}
+
   return (
     <div className="flex flex-col px-10 py-12">
       <div className="flex flex-col gap-24">
@@ -13,8 +57,8 @@ export function Login() {
             <ArrowLeft className="text-white" />
           </Link>
         </div>
-
-        <form className="flex flex-col items-center justify-center">
+    
+        <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
           <h1 className="text-dark-blue font-bold text-3xl">Login</h1>
           <p className="font-medium text-green-800 mt-2">Faça login para continuar</p>
 
