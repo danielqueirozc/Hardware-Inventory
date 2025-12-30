357a6d06504583b456e90f463362c40edddfa197
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-30 17:49:20 -0300

    style: adjust indentation

--- api/src/middlewares/verify-jwt.ts
+++ api/src/middlewares/verify-jwt.ts
@@ -1,8 +1,9 @@
+// middlewares/verify-jwt.ts
 import type { FastifyReply, FastifyRequest } from "fastify";
 
 export async function VerifyJwt(request: FastifyRequest, reply: FastifyReply) {
   try {
-    request.jwtVerify()
+    await request.jwtVerify()
   } catch (error) {
     return reply.status(401).send({ message: 'Unauthorized' })
   }
