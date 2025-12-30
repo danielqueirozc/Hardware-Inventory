03c684ec84f36a5293631bcfa68bc8fbf1e22631
danielqueirozc <danielqueiroz.job@gmail.com>
2025-12-30 17:50:52 -0300

    feat: created private e public route and useEffect verify

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,96 @@
 import { Introduction } from "./components/introduction";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Login } from "./components/login";
 import { Register } from "./components/register";
 import { Dashboard } from "./components/dashboard";
 import { Computer } from "./components/computer";
+import { useAuthStore } from "./context/auth-store";
+import { useEffect, useState } from "react";
 
+function PrivateRoute ({ children }: { children: React.ReactNode }) {
+const { isAuthenticated } = useAuthStore()
+
+  return isAuthenticated ?  <>{children}</> : <Navigate to="/login" replace />
+}
+
+function PublicRoute({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated } = useAuthStore()
+
+  return isAuthenticated ? <Navigate to='/dashboard' replace /> : <>{children}</>
+}
+ 
 export function App() {
+  // const { token, checkAuth } = useAuthStore()
+  const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+    async function verify() {
+      // Aguarda a hidratação terminar
+      await useAuthStore.persist.rehydrate()
+      
+      const token = useAuthStore.getState().token
+      
+      console.log('Token após rehydrate:', token)
+      
+      if (token) {
+        const checkAuth = useAuthStore.getState().checkAuth
+        await checkAuth()
+      }
+      
+      setIsLoading(false)
+    }
+    
+    verify()
+  }, [])
+
+  if (isLoading) return <p>Carregando...</p>
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Introduction />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/computer" element={<Computer />} />
+
+       <Route path="/" element={<Introduction />} />
+
+        <Route 
+          path="/introduction" 
+          element={
+            <PublicRoute>
+              <Introduction />
+            </PublicRoute>
+          } 
+        />
+        <Route
+          path="/login" 
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          } 
+        />
+        <Route
+          path="/register" 
+          element={
+            <PublicRoute>
+              <Register />
+            </PublicRoute>
+          } 
+        />
+        <Route 
+          path="/dashboard" 
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/computer" 
+          element={
+            <PrivateRoute>
+              <Computer />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
     </BrowserRouter>
   )
