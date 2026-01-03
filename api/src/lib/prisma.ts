b636f81b9108e3ea3b31e5ec1b041d8ef4a9e6b5
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-03 15:23:07 -0300

    fix: add validation connection-string

--- api/src/lib/prisma.ts
+++ api/src/lib/prisma.ts
@@ -4,7 +4,15 @@ import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
+if (!connectionString || connectionString === 'undefined') {
+  console.error('DATABASE_URL não está definida no arquivo .env')
+  throw new Error('DATABASE_URL não está definida')
+}
+
 const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient({
+  adapter,
+  log: ['query', 'error', 'warn']
+})
 
 export { prisma }
