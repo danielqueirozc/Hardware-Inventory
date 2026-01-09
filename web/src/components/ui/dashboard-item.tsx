56316624c0d56d9eda8b92257c94b080d77a05d8
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-09 15:01:23 -0300

    feat: add state get-items-by-type and create handle-click function

--- web/src/components/ui/dashboard-item.tsx
+++ web/src/components/ui/dashboard-item.tsx
@@ -1,15 +1,26 @@
+import type { ItemType } from "@/@types";
+import { useInventoryStore } from "@/context/inventory-store";
 import type { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 interface DashboardItemProps extends React.ComponentProps<'div'> {
   amount: number
   type: string
+  itemType: ItemType
   icon: ReactNode
 }
 
-export function DashboardItem({ amount, type, icon, ...props  }: DashboardItemProps) {
+
+export function DashboardItem({ amount, type, itemType, icon, ...props  }: DashboardItemProps) {
+  const { getItemsByType } = useInventoryStore()
+
+  function handleClick() {
+    console.log('DashboardItem clicked:', itemType)
+    getItemsByType(itemType)
+  }
+
   return (
-    <Link to="/computer"className="flex justify-between items-center pl-4 py-4 pr-10 bg-white rounded-lg h-18 border border-gray-700 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)]">
+    <Link {...props} onClick={handleClick} to={`/items/${itemType}`} className="flex justify-between items-center pl-4 py-4 pr-10 bg-white rounded-lg h-18 border border-gray-700 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)]">
       <div className="flex gap-4">
         <div className="self-stretch w-0.75 bg-green rounded-lg"/>
 
@@ -19,7 +30,7 @@ export function DashboardItem({ amount, type, icon, ...props  }: DashboardItemPr
         </div>
       </div>
       
-      <div {...props} className="flex items-center justify-center bg-green-300 rounded-full h-12 w-12">
+      <div className="flex items-center justify-center bg-green-300 rounded-full h-12 w-12">
         {icon}
       </div>
     </Link>
