a0fd545d6d00472fc70fdde25041937363290f23
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-09 15:27:51 -0300

    feat: create item and item-filter type

--- web/src/@types/index.d.ts
+++ web/src/@types/index.d.ts
@@ -1,10 +1,24 @@
-export interface RegisterType {
-  name: string
-  email: string
-  password: string
-}
+  export interface RegisterType {
+    name: string
+    email: string
+    password: string
+  }
 
-export interface LoginType {
-  email: string
-  password: string
-}
+  export interface LoginType {
+    email: string
+    password: string
+  }
+
+  export enum ItemType {
+    Computer = 'Computer',
+    Component = 'Component',
+    Notebook = 'Notebook',
+    Materials = 'Materials',
+    Cables = 'Cables'
+  }
+
+  export enum ItemFilterType {
+    Lab_Línguas,
+    Lab_Informática,
+    Lab_Hardware 
+  }
