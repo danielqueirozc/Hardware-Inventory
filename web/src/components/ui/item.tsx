268a56d6944cfab00cba7c97e20baf0b03df5625
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-12 18:05:02 -0300

    feat: add delete-item

--- web/src/components/ui/item.tsx
+++ web/src/components/ui/item.tsx
@@ -1,24 +1,33 @@
+import { useInventoryStore } from "@/context/inventory-store";
 import { Trash, Edit } from "lucide-react";
 
 interface ItemType {
   name: string
   code: string
   amount: number
+  id: string
 }
 
-export function Item({  name, code, amount }: ItemType) {
+export function Item({  name, code, amount, id }: ItemType) {
+  const { deleteItem } = useInventoryStore()
+
   return (
     <div className="flex flex-col border-x border-gray-300 rounded-lg bg-white shadow-[0_4px_6px_-1px] shadow-gray-400">
       <div className="flex items-center justify-between border-b-3 border-gray-300 pl-5 pr-8 py-2">
         <span className="font-bold text-xs">{name}</span>
 
         <div className="flex gap-4">
-          <Trash 
-            className="text-red-400 w-4 h-4"  
-          />
-          <Edit 
-            className="text-green w-4 h-4"  
-          />
+          <button onClick={() => deleteItem(id)}>
+            <Trash 
+              className="text-red-400 w-4 h-4"  
+            />
+          </button>
+          
+          <button>
+            <Edit 
+              className="text-green w-4 h-4"  
+            />
+          </button>
         </div>
       </div>
       <div className="flex justify-between pl-5 pr-8 py-2 border-b border-gray-300 ">
