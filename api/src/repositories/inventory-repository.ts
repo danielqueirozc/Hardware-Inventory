e52dbc86dc97f479dde8103d3714c0fe41087dc6
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-24 16:21:04 -0300

    feat: create interface edit-item-input

--- api/src/repositories/inventory-repository.ts
+++ api/src/repositories/inventory-repository.ts
@@ -1,4 +1,4 @@
-import type { Item, ItemType, Prisma } from "../generated/prisma/client";
+import type { Item, ItemType, Prisma, Filter } from "../generated/prisma/client";
 
 export interface GetItemsQuantityResponse {
   Component: number
@@ -8,8 +8,17 @@ export interface GetItemsQuantityResponse {
   Cables: number
 }
 
+export interface EditItemInput {
+  id: string
+  name: string
+  amount: number
+  filters: Filter[]
+}
+
 export interface InventoryRepository {
   create(data: Prisma.ItemCreateInput): Promise<Item>
   getItemsQuantity(): Promise<GetItemsQuantityResponse>
   getItemsByType(type: ItemType): Promise<Item[]>
+  deleteItem(id: string): Promise<void>
+  editItem(data: EditItemInput): Promise<Item> // nao coloquei Prisma.ItemUpdateInput tem uma estrutura diferente para arrays de enums. A tipagem espera { set: [...] } na propriedade, mas você está recebendo um array simples.
 }
