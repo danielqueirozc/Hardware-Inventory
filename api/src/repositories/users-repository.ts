9a9a36522db39b4dd58bba8fc95fc4dbaf5ba7e8
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-24 16:23:41 -0300

    feat: create update-profile-image

--- api/src/repositories/users-repository.ts
+++ api/src/repositories/users-repository.ts
@@ -3,6 +3,7 @@ import type { Prisma, User } from "../generated/prisma/client";
 export interface UsersRepository {
     create(data: Prisma.UserCreateInput): Promise<User>
     changePassword(id: string, hashedPassword: string): Promise<User>
+    updateProfileImage(id: string, imageUrl: string): Promise<User>
     findByEmail(email:string): Promise<User | null>
     findById(id:string): Promise<User | null>
 }
