f3979b249ce20050022f2be5113069a9ce19563a
danielqueirozc <danielqueiroz.job@gmail.com>
2026-01-24 16:12:25 -0300

    feat: create return and console.logs

--- api/src/http/controller/change-password.ts
+++ api/src/http/controller/change-password.ts
@@ -2,32 +2,39 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 import { makeChangePasswordService } from "../../service/factories/make-change-password-service";
 
-export async function ChangePassword(request: FastifyRequest, response: FastifyReply) {
+export async function ChangePassword(request: FastifyRequest, reply: FastifyReply) {
   const bodySchema = z.object({
-    newPassword: z.string().min(4),
+    newPassword: z.string(),
   })
 
   try {
     const userId = (request.user as { sub: string }).sub
 
+    
+    console.log(request.body)
     const { newPassword } = bodySchema.parse(request.body)
 
     const changePasswordService = makeChangePasswordService()
+
+
+    console.log(newPassword)
 
     await changePasswordService.execute({
       id: userId,
       newPassword,
     })
+
+    return reply.status(201).send({ message: 'Password updated successfully' })
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return response.status(400).send({
+      return reply.status(400).send({
         message: "Validation error",
         issues: error.format(),
       })
     }
 
     if (error instanceof Error) {
-      return response.status(409).send({ message: error.message })
+      return reply.status(409).send({ message: error.message })
     }
   }
 }
