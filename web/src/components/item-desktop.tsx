8d0b94ec7d4d5d45822d3301eabe783433aa166b
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 10:42:32 -0300

    feat: create item-desktop

---
+++ web/src/components/item-desktop.tsx
@@ -0,0 +1,29 @@
+import { Pencil, Trash2 } from "lucide-react";
+
+interface ItemDesktopType {
+  name: string
+  code: string
+  amount: number
+  id: string
+} 
+
+export function ItemDesktop({ code, name, amount, id }: ItemDesktopType) {
+
+  return (
+    <tr className="border-b border-gray-300">
+      <td className="py-4 px-4 text-gray-600">{code}</td>
+      <td className="py-4 px-4 text-gray-600">{name}</td>
+      <td className="py-4 px-4 text-gray-600">{amount}</td>
+      <td className="py-4 px-4">
+        <button className="cursor-pointer">
+          <Pencil className="text-green-500 w-5 h-5" />
+        </button>
+      </td>
+      <td className="py-4 px-4">
+        <button className="cursor-pointer">
+          <Trash2 className="text-red-400 w-5 h-5" />
+        </button>
+      </td>
+    </tr>
+  )
+}
