f235bf1d0d3912db7d8f0303553685f449551911
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 12:14:21 -0300

    feat: add SearchInputMobile

--- web/src/components/types/materials.tsx
+++ web/src/components/types/materials.tsx
@@ -1,38 +1,31 @@
 import { ArrowLeft, Search } from "lucide-react";
 import { Link } from "react-router-dom";
-import { Item } from "../ui/item";
+import { Item } from "../mobile/item-mobile";
 import { useInventoryStore } from "@/context/inventory-store";
 import { DialogAddNew } from "../dialog-add-new-item";
+import { SearchInputMobile } from "../ui/search-input-mobile";
 
 export function Materials() {
   const { itemsByType } = useInventoryStore()
 
   return (
     <div className="h-full bg-gray-200">
-       <header className="flex items-center justify-items-start bg-green px-10 py-4">
+       <header className="flex items-center justify-items-start bg-green px-10 py-4 md:py-8">
         <div className="flex items-center justify-center gap-4">
           <Link 
-            className="text-white w-10 h-10 bg-green-700 rounded-full flex items-center justify-center"
+            className="text-white w-10 md:w-15 h-10 md:h-15 bg-green-700 rounded-full flex items-center justify-center"
             to="/dashboard"
           >
-             <ArrowLeft />
+             <ArrowLeft className="md:w-8 md:h-8" />
           </Link>
-          <span className="font-semibold text-white">Materiais</span>
+          <span className="font-semibold text-white md:text-xl">Materiais</span>
         </div>
       </header>
 
       <div className="flex flex-col px-10 py-6 h-full">
         <div className="flex gap-4 justify-between items-center w-full h-8">
-          <div className="flex items-center relative w-full border border-green rounded-lg h-8">
-            <Search
-              className="absolute left-3 text-gray-600 w-5 h-5 pointer-events-none"
-            />
-            <input
-              className="pl-9 placeholder:text-gray-600 placeholder:font-medium w-full h-full rounded-lg focus:outline-none focus:ring-2 focus:ring-green"
-              type="text"
-              placeholder="Pesquisar"
-            />
-          </div>
+          
+          <SearchInputMobile />
 
           <div className="w-px bg-gray-700 h-4/5" />
 
@@ -48,7 +41,7 @@ export function Materials() {
               id={item.id}
               code={item.code}
               amount={item.amount}
-              filter={item.filter}
+              filters={item.filters}
             />
           ))}
         </div>
