7757c21689c2280ae7eca9e7b4c6cb929f2641e0
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 10:42:14 -0300

    style: update layout and visual styles

--- web/src/components/introduction.tsx
+++ web/src/components/introduction.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export function Introduction() {
   return (
-   <div className="flex flex-col h-screen">
-     <div className="relative bg-[url('wallpaper-mobile.jpg')] bg-cover bg-no-repeat bg-center h-3/5 rounded rounded-b-[40px]">
-     <div className="absolute bg-green/50 w-full h-full rounded rounded-b-[40px]" />
+   <div className="flex flex-col lg:flex-row h-screen">
+     <div className="relative lg:w-6/10 lg:h-full bg-[url('wallpaper-mobile.jpg')] bg-cover bg-no-repeat bg-center h-3/5 rounded rounded-b-[40px] lg:rounded-bl-none lg:rounded-r-[50px]">
+     <div className="absolute bg-green/50 w-full h-full rounded rounded-b-[40px] lg:rounded-bl-none lg:rounded-r-[50px]" />
       <img 
         className="relative h-12 mt-8 ml-5 z-10"
         src="/logo-fundo-escuro.svg" 
@@ -12,13 +12,13 @@ export function Introduction() {
       />
     </div>
 
-    <div className="bg-white flex justify-center items-center py-12 px-10">
+    <div className="bg-white flex justify-center items-center py-12 px-10 md:mt-8">
       <div className="flex flex-col gap-8 justify-center items-center">
         <h1 className="font-bold text-green text-3xl">DQCstock</h1>
-        <p className="font-medium text-gray text-lg text-center">Armazene com segurança uma variedade de equipamenos TI.</p>
+        <p className="font-medium text-gray text-lg md:text-xl text-center">Armazene com segurança uma variedade de equipamenos TI.</p>
         <Link
           to="/login"
-          className="font-medium text-lg text-white py-4 bg-green w-full rounded-lg flex items-center justify-center"
+          className="font-medium text-lg md:text-xl text-white py-4 bg-green w-full rounded-lg flex items-center justify-center"
           type="button"
         >
           Fazer login
