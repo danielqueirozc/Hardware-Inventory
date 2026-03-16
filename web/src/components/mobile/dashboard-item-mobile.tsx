3be64c220578c0fb0603ee2ccc171ff830c32d56
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 10:52:40 -0300

    feat: create dashboard-item-mobile

---
+++ web/src/components/mobile/dashboard-item-mobile.tsx
@@ -0,0 +1,51 @@
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
+import { useInventoryStore } from "@/context/inventory-store";
+import { SearchInputMobile } from "../ui/search-input-mobile";
+import { DialogAddNew } from "../dialog-add-new-item";
+import { ItemMobile } from "./item-mobile";
+
+export function DashboardItemMobile() {
+  const { itemsByType } = useInventoryStore()
+
+  return (
+    <div className="h-auto bg-gray-200">
+       <header className="flex items-center justify-items-start bg-green px-10 py-4 md:py-8">
+        <div className="flex items-center justify-center gap-4">
+          <Link 
+            className="text-white w-10 md:w-15 h-10 md:h-15 bg-green-700 rounded-full flex items-center justify-center"
+            to="/dashboard"
+          >
+             <ArrowLeft className="md:w-8 md:h-8" />
+          </Link>
+          <span className="font-semibold text-white md:text-xl">Computador</span>
+        </div>
+      </header>
+
+      <div className="flex flex-col px-10 py-6 h-full md:mt-8">
+        <div className="flex gap-4 justify-between items-center w-full h-8">
+         
+        <SearchInputMobile />
+
+        <div className="w-px bg-gray-700 h-4/5" />
+
+        <DialogAddNew />
+         
+        </div>
+
+        <div className="flex flex-col gap-6 md:gap-10 mt-8">
+          {itemsByType.map(item => (
+            <ItemMobile 
+              key={item.code}
+              name={item.name}
+              id={item.id}
+              code={item.code}
+              amount={item.amount}
+              filters={item.filters}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
