66cda01cd41ae4ef0c9b340d6dc7f3c9eba4c18c
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 12:17:57 -0300

    feat: create info-user

---
+++ web/src/components/ui/info-user.tsx
@@ -0,0 +1,26 @@
+import { useAuthStore } from "@/context/auth-store";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
+export function InfoUser() {
+  const { user } = useAuthStore()
+
+  const profileImageUrl = user?.imageUrl 
+    ? `http://localhost:3333${user.imageUrl}` 
+    : "https://github.com/shadcn.png"
+
+  return (
+     <div className="flex items-center gap-6">
+        <div className="flex flex-col">
+          <strong>
+            {user?.name}
+          </strong>
+          <span>{user?.email}</span>
+        </div>
+
+        <Avatar className="w-16 md:w-28 lg:w-12 h-16 md:h-28 lg:h-12">
+          <AvatarImage src={profileImageUrl} />
+          <AvatarFallback>{user?.name?.charAt(0) || 'CN'}</AvatarFallback>
+        </Avatar>
+      </div>
+  )
+}
