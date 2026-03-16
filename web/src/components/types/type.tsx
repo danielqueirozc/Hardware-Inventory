4745bd022be66f4fdb2e7c2b6274936c6d5d37f2
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 12:16:24 -0300

    feat: create type

---
+++ web/src/components/types/type.tsx
@@ -0,0 +1,17 @@
+export function Type() {
+  return (
+     <div className="flex flex-1 flex-col bg-[#FAFAFA]">
+        <header className="flex items-center justify-between bg-white px-14 py-8 border-b-3 border-gray-300">
+          <div className="flex gap-4 items-center">
+            <h1 className="text-green text-3xl font-bold">Components</h1>
+            <p>/</p>
+            <h3 className="text-2xl">Estoque</h3>
+          </div>
+
+          <div className="flex">
+            name
+          </div>
+        </header>
+      </div>
+  )
+}
