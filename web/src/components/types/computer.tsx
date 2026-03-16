e249f2302b41dc06806c1deb386bc3e0e116b072
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 12:13:53 -0300

    feat: add Sidebar, DashboardItem, DashboardItemMobile and handleResize

--- web/src/components/types/computer.tsx
+++ web/src/components/types/computer.tsx
@@ -1,52 +1,26 @@
-import { ArrowLeft } from "lucide-react";
-import { Link } from "react-router-dom";
-import { Item } from "../ui/item";
-import { useInventoryStore } from "@/context/inventory-store";
-import { DialogAddNew } from "../dialog-add-new-item";
-import { SearchInputMobile } from "../ui/search-input-mobile";
-import { ItemType } from "@/@types";
+import { useEffect, useState } from "react";
+import { DashboardItem } from "../dashboard-item";
+import { DashboardItemMobile } from "../mobile/dashboard-item-mobile";
+import { Sidebar } from "../sidebar";
 
 export function Computers() {
-  const { itemsByType } = useInventoryStore()
+  const [isLargeScreen, setIsLargeScreen] = useState<boolean>(window.innerWidth >= 1024)
 
+  useEffect(() => {
+    const handleResize = () => setIsLargeScreen(window.innerWidth >= 1024)
+    window.addEventListener('resize', handleResize)
+  }, [])
 
   return (
-    <div className="h-auto bg-gray-200">
-       <header className="flex items-center justify-items-start bg-green px-10 py-4">
-        <div className="flex items-center justify-center gap-4">
-          <Link 
-            className="text-white w-10 h-10 bg-green-700 rounded-full flex items-center justify-center"
-            to="/dashboard"
-          >
-             <ArrowLeft />
-          </Link>
-          <span className="font-semibold text-white">Computador</span>
-        </div>
-      </header>
-
-      <div className="flex flex-col px-10 py-6 h-full">
-        <div className="flex gap-4 justify-between items-center w-full h-8">
-         
-         <SearchInputMobile />
-
-          <div className="w-px bg-gray-700 h-4/5" />
-
-        <DialogAddNew />
-         
-        </div>
-
-        <div className="flex flex-col gap-6 mt-8">
-          {itemsByType.map(item => (
-            <Item 
-              key={item.code}
-              name={item.name}
-              id={item.id}
-              code={item.code}
-              amount={item.amount}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+   <>
+    {isLargeScreen ? (
+     <div className="flex h-screen border-2 border-black">
+      <Sidebar />
+      <DashboardItem type="Computadores" />
+     </div>
+    ) : (
+      <DashboardItemMobile />
+    )}
+   </>
   )
 }
