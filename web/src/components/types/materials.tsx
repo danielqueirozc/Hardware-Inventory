275ab4a6e18a1aa0636389cca8efc460c3f4a704
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-16 21:20:21 -0300

    feat: add Sidebar, DashboardItem and DashboardItemMobile

--- web/src/components/types/materials.tsx
+++ web/src/components/types/materials.tsx
@@ -1,51 +1,26 @@
-import { ArrowLeft, Search } from "lucide-react";
-import { Link } from "react-router-dom";
-import { Item } from "../mobile/item-mobile";
-import { useInventoryStore } from "@/context/inventory-store";
-import { DialogAddNew } from "../dialog-add-new-item";
-import { SearchInputMobile } from "../ui/search-input-mobile";
+import { useEffect, useState } from "react";
+import { DashboardItem } from "../dashboard-item";
+import { DashboardItemMobile } from "../mobile/dashboard-item-mobile";
+import { Sidebar } from "../sidebar";
 
 export function Materials() {
-  const { itemsByType } = useInventoryStore()
+  const [isLargeScreen, setIsLargeScreen] = useState<boolean>(window.innerWidth >= 1024)
+
+  useEffect(() => {
+    const handleResize = () => setIsLargeScreen(window.innerWidth >= 1024)
+    window.addEventListener('resize', handleResize)
+  }, [])
 
   return (
-    <div className="h-full bg-gray-200">
-       <header className="flex items-center justify-items-start bg-green px-10 py-4 md:py-8">
-        <div className="flex items-center justify-center gap-4">
-          <Link 
-            className="text-white w-10 md:w-15 h-10 md:h-15 bg-green-700 rounded-full flex items-center justify-center"
-            to="/dashboard"
-          >
-             <ArrowLeft className="md:w-8 md:h-8" />
-          </Link>
-          <span className="font-semibold text-white md:text-xl">Materiais</span>
-        </div>
-      </header>
-
-      <div className="flex flex-col px-10 py-6 h-full">
-        <div className="flex gap-4 justify-between items-center w-full h-8">
-          
-          <SearchInputMobile />
-
-          <div className="w-px bg-gray-700 h-4/5" />
-
-          <DialogAddNew />
-         
-        </div>
-
-        <div className="flex flex-col gap-6 mt-8">
-          {itemsByType.map(item => (
-            <Item 
-              key={item.code}
-              name={item.name}
-              id={item.id}
-              code={item.code}
-              amount={item.amount}
-              filters={item.filters}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+   <>
+    {isLargeScreen ? (
+     <div className="flex h-screen">
+      <Sidebar />
+      <DashboardItem type="Materiais" />
+     </div>
+    ) : (
+      <DashboardItemMobile />
+    )}
+   </>
   )
 }
