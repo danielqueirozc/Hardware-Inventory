f852059885a5a4f07ff51b351e0d140fd28ddef6
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-19 18:34:59 -0300

    feat: add changeName and changeEmail

--- api/src/repositories/users-repository.ts
+++ api/src/repositories/users-repository.ts
@@ -2,6 +2,8 @@ import type { Prisma, User } from "../generated/prisma/client";
 
 export interface UsersRepository {
     create(data: Prisma.UserCreateInput): Promise<User>
+    changeName(id: string, name: string): Promise<User>
+    changeEmail(id: string, email: string): Promise<User>
     changePassword(id: string, hashedPassword: string): Promise<User>
     updateProfileImage(id: string, imageUrl: string): Promise<User>
     findByEmail(email:string): Promise<User | null>
