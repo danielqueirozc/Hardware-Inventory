d57724f410eff8662f2f37d006b1b540e9120f16
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-23 20:49:23 -0300

    feat: create get-item-by-filter

---
+++ api/src/service/get-item-by-filter.ts
@@ -0,0 +1,24 @@
+import type { Item } from "../generated/prisma/client";
+import type { Filter, ItemType } from "../generated/prisma/enums";
+import type { InventoryRepository } from "../repositories/inventory-repository";
+
+interface GetItemByFilterRequest {
+  filter: Filter[]
+  type: ItemType
+}
+
+interface GetItemByFilterResponse {
+  item: Item[]
+}
+
+export class GetItemByFilter {
+  constructor (private inventoryRepository: InventoryRepository) {}
+
+  async execute({ filter, type }: GetItemByFilterRequest): Promise<GetItemByFilterResponse> {
+    const item = await this.inventoryRepository.getItemsByFilter(filter, type)
+
+    console.log('resultado do service',item)
+
+    return { item }
+  }
+}
