68a1503c47fb79964d9a453474d299d0df401a94
danielqueirozc <danielqueiroz.job@gmail.com>
2026-03-23 20:47:22 -0300

    feat: add feat: create get-item-by-filter

--- api/src/repositories/inventory-repository.ts
+++ api/src/repositories/inventory-repository.ts
@@ -12,13 +12,14 @@ export interface EditItemInput {
   id: string
   name: string
   amount: number
-  filters: Filter[]
+  filter: Filter[]
 }
 
 export interface InventoryRepository {
   create(data: Prisma.ItemCreateInput): Promise<Item>
   getItemsQuantity(): Promise<GetItemsQuantityResponse>
   getItemsByType(type: ItemType): Promise<Item[]>
+  getItemsByFilter(filter: Filter[], type: ItemType): Promise<Item[]>
   deleteItem(id: string): Promise<void>
-  editItem(data: EditItemInput): Promise<Item> // nao coloquei Prisma.ItemUpdateInput tem uma estrutura diferente para arrays de enums. A tipagem espera { set: [...] } na propriedade, mas você está recebendo um array simples.
+  editItem(data: EditItemInput): Promise<Item> // nao coloquei Prisma.ItemUpdateInput tem uma estrutura diferente para arrays de enums. A tipagem espera { set: [...] } na propriedade, mas estou recebendo um array simples.
 }
