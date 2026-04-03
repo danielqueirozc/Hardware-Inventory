433fdc219bfde9301f7b4f554a1129affbc970bd
danielqueirozc <danielqueiroz.job.com>
2026-04-03 12:49:25 -0300

    feat: add searchItem

--- api/src/repositories/inventory-repository.ts
+++ api/src/repositories/inventory-repository.ts
@@ -20,6 +20,7 @@ export interface InventoryRepository {
   getItemsQuantity(): Promise<GetItemsQuantityResponse>
   getItemsByType(type: ItemType): Promise<Item[]>
   getItemsByFilter(filter: Filter[], type: ItemType): Promise<Item[]>
+  searchItem(q: string, type?: ItemType): Promise<Item[]>
   deleteItem(id: string): Promise<void>
   editItem(data: EditItemInput): Promise<Item> // nao coloquei Prisma.ItemUpdateInput tem uma estrutura diferente para arrays de enums. A tipagem espera { set: [...] } na propriedade, mas estou recebendo um array simples.
 }
