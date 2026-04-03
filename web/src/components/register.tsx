666e03e38a44a05695a561685fe7fbe56249088b
danielqueirozc <danielqueiroz.job@gmail.com>
2026-04-03 17:14:43 -0300

    chore: ignore unused error state variable

--- web/src/components/register.tsx
+++ web/src/components/register.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export function Register() {
-  const [ error, setError ] = useState('')
+  const [ _, setError ] = useState('')
 
   const { register } = useAuthStore()
   
