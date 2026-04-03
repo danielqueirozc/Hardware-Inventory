a95698207a353330de5d4180a4223e59df2cc830
danielqueirozc <danielqueiroz.job@gmail.com>
2026-04-03 17:15:24 -0300

    chore: remove const result

--- web/src/components/ui/change-password-form.tsx
+++ web/src/components/ui/change-password-form.tsx
@@ -64,7 +64,7 @@ export function ChangePasswordForm() {
 
 
     try {
-      const result = await authServie.changePassword(newPassword)
+      await authServie.changePassword(newPassword)
 
     } catch (error) {
       console.error('Erro ao trocar senha:', error)
